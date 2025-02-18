0ca4ef5093c6662108cda25c27dfa564268e3158
Marek Brzezinski <marek.j.brzezinski@gmail.com>
2025-02-18 21:30:04 +0100

    Groups

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 from constants import *
-from player import *
+from player import Player
 
 
 def main():
@@ -11,12 +11,19 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    drawable = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
+
+    Player.containers = (drawable, updatable)
+
     player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.update(dt)
+        updatable.update(dt)
+        for item in drawable:
+            item.draw(screen)
         screen.fill((0, 0, 0))
         player.draw(screen)
         pygame.display.flip()
